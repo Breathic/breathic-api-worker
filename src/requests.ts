2c7060083f7df0043e3d5cae65d4f2ad17e91a50
Andrus Asumets <andrus.asumets@gmail.com>
2023-04-05 23:06:13 +0300

    cleanup auth

--- src/requests.ts
+++ src/requests.ts
@@ -1,55 +1,59 @@
 import jwt from '@tsndr/cloudflare-worker-jwt';
-const Buffer = require('buffer/').Buffer;
+import { Buffer } from 'buffer';
 
 const uuid = () => {
     return Date.now().toString(36) + Math.random().toString(36).substring(2);
 };
 
 const validateDevice = async (
-    DEVICE_TOKEN: string,
-    KEY_ID: string,
-    KEY: string,
-    TEAM_ID: string,
-    BUNDLE_ID: string
+    deviceToken: string,
+    keyId: string,
+    key: string,
+    teamId: string,
+    bundleId: string
 ): Promise<Boolean> => {
     try {
-        const jwtHeaders = {
-            algorithm: 'ES256',
-            header: {
-                kid: KEY_ID
-            }
-        };
-        const jwToken = await jwt.sign(
+        const bearerToken = await jwt.sign(
             {
-                iss: TEAM_ID,
+                iss: teamId,
                 iat: Date.now() / 1000,
                 exp: (Date.now() + 600 * 1000) / 1000,
-                sub: BUNDLE_ID
+                sub: bundleId
             },
-            Buffer.from(String(KEY), 'base64').toString('ascii'),
-            jwtHeaders
+            Buffer.from(key, 'base64').toString('ascii'),
+            {
+                algorithm: 'ES256',
+                header: {
+                    kid: keyId
+                }
+            }
         );
         const payload = {
             timestamp: Date.now(),
             transaction_id: uuid(),
-            device_token: DEVICE_TOKEN
+            device_token: deviceToken
         };
         const url = `https://api.devicecheck.apple.com/v1/validate_device_token`;
         const headers = {
-            Authorization: `Bearer ${jwToken}`,
+            Authorization: `Bearer ${bearerToken}`,
             'Content-Type': 'application/json'
         };
         const request = new Request(
             url,
-            { method: 'POST', headers, body: JSON.stringify(payload) }
+            {
+                method: 'POST',
+                headers,
+                body: JSON.stringify(payload)
+            }
         );
+
         const response = await fetch(request);
         if (response.status === 200 && response.ok) {
             return true;
         }
     } catch (err) {
         console.log('validateDevice()', err);
-    };
+    }
 
     return false;
 };
